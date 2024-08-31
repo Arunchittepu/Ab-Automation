9b4d99704c964417db5bd2add0fa7c7378589c3f
NarasimhaReddy <narasimha@fabric.inc>
2024-08-31 07:57:56 +0530

    added key

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'zyj8fh',
   e2e: {
     baseUrl:"https://www.amazon.com",
+    defaultCommandTimeout: 60000, // Increase timeout
+    responseTimeout: 60000,
+    pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
+    env: {
+      CYPRESS_RECORD_KEY: 'b7071857-a207-4f33-b334-aa9e819fed0b',
+    }
   },
 });
