9f2d608355c583e142a400a1e1746792d479ce53
NarasimhaReddy <narasimha@fabric.inc>
2024-10-22 08:30:56 +0530

    config files added newly

---
+++ stage.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'zyj8fh',
+  defaultCommandTimeout: 60000, // Increase timeout
+    responseTimeout: 60000,
+    pageLoadTimeout: 120000,
+    chromeWebSecurity: false,
+    experimentalModifyObstructiveThirdPartyCode: true,
+    failOnStatusCode: false,
+    redirectionLimit: 100,
+    "chromeWebSecurity": false,
+   "experimentalSessionAndrigin": true,
+  e2e: {
+    baseUrl:"https://www.amazon.co.uk",
+    defaultCommandTimeout: 60000,    
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      
+    },
+    env: {
+      CYPRESS_RECORD_KEY: 'b7071857-a207-4f33-b334-aa9e819fed0b',
+    }
+  },
+});
